a112fcae66d02648446100f5262f6f61b9774372
Einsy <huzc818@qq.com>
2017-09-23 20:26:11 +0800

    update

--- src/android/socketServer.java
+++ src/android/socketServer.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
