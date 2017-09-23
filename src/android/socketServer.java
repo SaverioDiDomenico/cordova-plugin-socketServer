01cf1afd4d615c5c6c74926213ce0166afc1001f
Einsy <huzc818@qq.com>
2017-09-23 23:14:57 +0800

    debug

--- src/android/socketServer.java
+++ src/android/socketServer.java
@@ -17,6 +17,23 @@ import java.net.Socket;
 // Cordova
 import android.util.Log;
 
+
+import java.util.ArrayList;
+import java.net.URLEncoder;  
+
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+
+
 public class socketServer extends CordovaPlugin {
 	private String OutString;
 	ServerSocket myServerSocket;
