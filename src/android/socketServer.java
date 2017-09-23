97e7306f04238fbf38c14e829cac6ae8ccbb28a9
Einsy <huzc818@qq.com>
2017-09-23 20:17:34 +0800

    修复代码错误

--- src/android/socketServer.java
+++ src/android/socketServer.java
@@ -17,103 +17,103 @@ import java.net.Socket;
 import android.util.Log;
 
 public class socketServer extends CordovaPlugin {
-    private String OutString;
-    ServerSocket myServerSocket;
-    boolean ServerOn = true;
-    boolean ServerActivated = false;
-    
-    @Override
-    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        try {
-            if ("startServer".equals(action)) {
-                JSONObject arg_object = args.getJSONObject(0);
-                int port =  Integer.parseInt(arg_object.getString("port"));
-                if(ServerActivated){
-                    callbackContext.error("has been startServer");
-                    return false;
-                }
-                cordova.getThreadPool().execute(new Runnable() {
-                    public void run() {
-                        try{ 
-                            myServerSocket = new ServerSocket(port); 
-                            ServerActivated = true;
-                        } catch(IOException ioe) { 
-                            System.exit(-1); 
-                        }
+	private String OutString;
+	ServerSocket myServerSocket;
+	boolean ServerOn = true;
+	boolean ServerActivated = false;
+	
+	@Override
+	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+		try {
+			if ("startServer".equals(action)) {
+				JSONObject arg_object = args.getJSONObject(0);
+				int port =  Integer.parseInt(arg_object.getString("port"));
+				if(ServerActivated){
+					callbackContext.error("has been startServer");
+					return false;
+				}
+				cordova.getThreadPool().execute(new Runnable() {
+					public void run() {
+						try{ 
+							myServerSocket = new ServerSocket(port); 
+							ServerActivated = true;
+						} catch(IOException ioe) { 
+							System.exit(-1); 
+						}
 
-                        while(ServerOn) {
-                            try{ 
-                                Socket clientSocket = myServerSocket.accept();
-                                ClientServiceThread ClientThread = new ClientServiceThread(clientSocket, callbackContext);
-                                ClientThread.start();
-                            } catch(IOException ioe) {
-                                ioe.printStackTrace();
-                            }
-                        }
-                        try {
-                            myServerSocket.close();
-                        } catch(Exception e) {
-                            System.exit(-1); 
-                        }
-                        //callbackContext.success();
-                    }
-                });
-                return true;
-            } else {
-                callbackContext.error(action + "not supported");
-                return false;
-            }
-        } catch (Exception e) {
-            callbackContext.error(e.getMessage());
-            return false;
-        }
-    }
-    
-    class ClientServiceThread extends Thread { 
-        Socket myClientSocket;
-        boolean m_bRunThread = true; 
-        CallbackContext myCallbackContext;
+						while(ServerOn) {
+							try{ 
+								Socket clientSocket = myServerSocket.accept();
+								ClientServiceThread ClientThread = new ClientServiceThread(clientSocket, callbackContext);
+								ClientThread.start();
+							} catch(IOException ioe) {
+								ioe.printStackTrace();
+							}
+						}
+						try {
+							myServerSocket.close();
+						} catch(Exception e) {
+							System.exit(-1); 
+						}
+						//callbackContext.success();
+					}
+				});
+				return true;
+			} else {
+				callbackContext.error(action + "not supported");
+				return false;
+			}
+		} catch (Exception e) {
+			callbackContext.error(e.getMessage());
+			return false;
+		}
+	}
+	
+	class ClientServiceThread extends Thread { 
+		Socket myClientSocket;
+		boolean m_bRunThread = true; 
+		CallbackContext myCallbackContext;
 
-        public ClientServiceThread() { 
-            super(); 
-        } 
+		public ClientServiceThread() { 
+			super(); 
+		} 
 
-        ClientServiceThread(Socket socket, CallbackContext callbackContext) {
-            myClientSocket = socket; 
-            myCallbackContext=callbackContext;
-        }
+		ClientServiceThread(Socket socket, CallbackContext callbackContext) {
+			myClientSocket = socket; 
+			myCallbackContext=callbackContext;
+		}
 
-        public void run() {
-            try{
-                InputStream input=myClientSocket.getInputStream();
-                OutputStream output = myClientSocket.getOutputStream();
-                byte buffer[] = new byte[1024 * 10];
-                int count=0;
-                while(m_bRunThread) {
-                    if(!ServerOn){ 
-                        m_bRunThread = false;
-                    }
-                    else{
-                        count = input.read(buffer);
-                        if(count==-1){break;}
-                        else{
-                            CallbackContext.success(buffer);
-                        }
-                    }
-                } 
-            } 
-            catch(Exception e) { 
-                e.printStackTrace(); 
-            } 
-            finally{ 
-                try{
-                    in.close(); 
-                    myClientSocket.close();
-                } 
-                catch(IOException ioe) { 
-                    ioe.printStackTrace(); 
-                } 
-            } 
-        } 
-    }   
+		public void run() {
+			try{
+				InputStream input=myClientSocket.getInputStream();
+				OutputStream output = myClientSocket.getOutputStream();
+				byte buffer[] = new byte[1024 * 10];
+				int count=0;
+				while(m_bRunThread) {
+					if(!ServerOn){ 
+						m_bRunThread = false;
+					}
+					else{
+						count = input.read(buffer);
+						if(count==-1){break;}
+						else{
+							myCallbackContext.success(buffer);
+						}
+					}
+				} 
+			} 
+			catch(Exception e) { 
+				e.printStackTrace(); 
+			} 
+			finally{ 
+				try{
+					input.close(); 
+					myClientSocket.close();
+				} 
+				catch(IOException ioe) { 
+					ioe.printStackTrace(); 
+				} 
+			} 
+		} 
+	}   
 }
