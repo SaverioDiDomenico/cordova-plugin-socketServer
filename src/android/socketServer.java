c9bde53a380b90bb182c45d08e6199564f7679ac
Einsy <huzc818@qq.com>
2017-09-24 20:27:20 +0800

    优化逻辑

--- src/android/socketServer.java
+++ src/android/socketServer.java
@@ -19,10 +19,13 @@ import java.net.Socket;
 // Cordova
 import android.util.Log;
 import java.util.UUID;
+import android.util.Base64;
 import java.util.HashMap;
 
 public class socketServer extends CordovaPlugin {
+	private String OutString;
 	ServerSocket myServerSocket;
+	boolean ServerOn = true;
 	boolean ServerActivated = false;
 	HashMap<String, Socket> socketHashMap;
 
@@ -35,22 +38,23 @@ public class socketServer extends CordovaPlugin {
 					callbackContext.error("has been startServer");
 					return false;
 				}
+				socketHashMap = new HashMap<String, Socket>();
 				this.startServer(port,callbackContext);
 				return true;
-			} else if("write".equals(action)){
+			}
+			else if("write".equals(action)){
 				final String uuid = args.getString(0); 
 				String data = args.getString(1);
 				if(socketHashMap.containsKey(uuid)){
 					Socket socket=socketHashMap.get(uuid);
 					OutputStream output=socket.getOutputStream();
-					output.write(data.getBytes());
+					output.write(data.getBytes(),0,data.getBytes().length);
 					return true;
 				} else {
 					return false;
 				}
-				
 			}
- 			else if("disconnect".equals(action)){
+			else if("disconnect".equals(action)){
 				final String uuid = args.getString(0); 
 				String data = args.getString(1);
 				if(socketHashMap.containsKey(uuid)){
@@ -60,7 +64,7 @@ public class socketServer extends CordovaPlugin {
 					return false;
 				}
 			}
- 			else if("close".equals(action)){
+			else if("close".equals(action)){
 				myServerSocket.close();
 				return true;
 			}
@@ -74,8 +78,7 @@ public class socketServer extends CordovaPlugin {
 		}
 	}
 
-	public void startServer(final int port, final CallbackContext callbackContext){
-		socketHashMap = new HashMap<String, Socket>();
+	private void startServer(final int port, final CallbackContext callbackContext){
 		cordova.getThreadPool().execute(new Runnable() {
 			public void run() {
 				try{ 
@@ -84,40 +87,47 @@ public class socketServer extends CordovaPlugin {
 					while(true) {
 						try{ 
 							Socket clientSocket = myServerSocket.accept();
+							clientSocket.setSoTimeout(10000);
 							String uuid = UUID.randomUUID().toString();
 							socketHashMap.put(uuid,clientSocket);
 							ClientServiceThread ClientThread = new ClientServiceThread(uuid, clientSocket, callbackContext);
 							ClientThread.start();
 						} catch(IOException ioe) {
 							ioe.printStackTrace();
-							callbackContext.error(ioe.getMessage());
 						}
 					}
-				} catch(IOException ioe) {
-					callbackContext.error(ioe.getMessage());
+
+				} catch(IOException ioe) { 
 					ioe.printStackTrace();
 					//System.exit(-1); 
+				} finally {
+					if (myServerSocket != null) {
+						try {
+							myServerSocket.close();
+							myServerSocket = null;
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
 				}
 			}
 		});
 	}
 
-    public void sendPluginResult(CallbackContext callbackContext,JSONObject obj){
+	public void sendPluginResult(CallbackContext callbackContext,JSONObject obj){
 		PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, obj);
 		pluginResult.setKeepCallback(true);
 		callbackContext.sendPluginResult(pluginResult);
-    }
-	
+	}
 
-	class ClientServiceThread extends Thread {
+	class ClientServiceThread extends Thread { 
 		Socket myClientSocket;
-		boolean m_bRunThread = true; 
 		CallbackContext myCallbackContext;
-		String myuuid;
+		String myuuid="";
 
-		public ClientServiceThread() {
+		public ClientServiceThread() { 
 			super();
-		}
+		} 
 
 		ClientServiceThread(String uuid, Socket socket, CallbackContext callbackContext) {
 			myClientSocket = socket; 
@@ -130,23 +140,36 @@ public class socketServer extends CordovaPlugin {
 				InputStream input=myClientSocket.getInputStream();
 				OutputStream output = myClientSocket.getOutputStream();
 				byte buffer[] = new byte[1024 * 4];
-				int count=0;
+				int len=0;
 				JSONObject eventObject = new JSONObject();
 				eventObject.put("type", "connect");
 				eventObject.put("socketId", myuuid);
 				socketServer.this.sendPluginResult(myCallbackContext,eventObject);
-				int len =-1;
-				while((len = input.read(buffer))!= -1) {
-					JSONObject dataObject = new JSONObject();
-					dataObject.put("type", "data");
-					dataObject.put("buffer", buffer);
-					dataObject.put("socketId", myuuid);
-					dataObject.put("HostName", myClientSocket.getInetAddress().getHostName());
-					dataObject.put("HostAddress", myClientSocket.getInetAddress().getHostAddress());
-					//PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, buffer);
-					socketServer.this.sendPluginResult(myCallbackContext,dataObject);
+				//while((len = input.read(buffer))!= -1) {
+				while(true) {
+					len = input.read(buffer);
+					if(len==-1){
+						JSONObject closeObject0 = new JSONObject();
+						closeObject0.put("type", "closeData");
+						closeObject0.put("socketId", myuuid);
+						socketServer.this.sendPluginResult(myCallbackContext,closeObject0);	
+						break;
+					}
+					else{
+						JSONObject dataObject = new JSONObject();
+						dataObject.put("type", "data");
+						dataObject.put("length", len);
+						String str=new String(buffer, 0, len);
+						dataObject.put("buffer", Base64.encodeToString(str.getBytes("UTF-8"), Base64.NO_WRAP));
+						dataObject.put("socketId", myuuid);
+						dataObject.put("HostName", myClientSocket.getInetAddress().getHostName());
+						dataObject.put("HostAddress", myClientSocket.getInetAddress().getHostAddress());
+						PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, dataObject);
+						pluginResult.setKeepCallback(true);
+						myCallbackContext.sendPluginResult(pluginResult);
+					}
 				}
-
+				myClientSocket.close();
 				JSONObject closeObject = new JSONObject();
 				closeObject.put("type", "close");
 				closeObject.put("socketId", myuuid);
@@ -162,8 +185,8 @@ public class socketServer extends CordovaPlugin {
 				} 
 				catch(IOException ioe) {
 					ioe.printStackTrace();
-				} 
-			} 
-		} 
-	}   
+				}
+			}
+		}
+	}
 }
